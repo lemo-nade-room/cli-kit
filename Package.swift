a83a0737de3066345922c671881b63dd77e6d75a
tanaka <lemo.nade.engineer.room@gmail.com>
2024-01-22 10:10:53 +0900

    Swift Commandへの依存を追加

--- Package.swift
+++ Package.swift
@@ -19,6 +19,8 @@ let package = Package(
     .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
     // 🪶 Fluent driver for SQLite.
     .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+    // Swift Command
+    .package(url: "https://github.com/Zollerboy1/SwiftCommand.git", from: "1.4.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -28,6 +30,7 @@ let package = Package(
       dependencies: [
         .product(name: "Vapor", package: "vapor"),
         .product(name: "Fluent", package: "fluent"),
+        .product(name: "SwiftCommand", package: "SwiftCommand"),
       ]
     ),
     .testTarget(
---
+++ Tests/CLIKitTests/SwiftCommandTests.swift
@@ -0,0 +1,41 @@
+import SwiftCommand
+import Vapor
+import XCTest
+
+@testable import CLIKit
+
+final class SwiftCommandTests: XCTestCase {
+  func test_can_file_create_and_remove() async throws {
+    // Arrange
+    var env: Environment = .testing
+    env.commandInput = CommandInput(arguments: ["<endpoint>", "create-file", "hello.txt"])
+    let app = Application(env)
+    defer { app.shutdown() }
+
+    final class CreateFileCommand: AsyncCommand {
+      struct Signature: CommandSignature {
+        @Argument(name: "file path", help: "The path to the file to create")
+        var filePath: String
+      }
+      let help = "Create a file at the given path"
+      func run(using context: ConsoleKitCommands.CommandContext, signature: Signature) async throws
+      {
+        let output = try await Command.findInPath(withName: "touch")!
+          .addArgument(signature.filePath)
+          .output
+        assert(output.status.terminatedSuccessfully)
+      }
+    }
+
+    let command = CreateFileCommand()
+    app.asyncCommands.use(command, as: "create-file")
+
+    // Act
+    try await app.cliKit()
+
+    // Assert
+    let fileManager = FileManager()
+    XCTAssertTrue(fileManager.fileExists(atPath: "hello.txt"))
+    try fileManager.removeItem(atPath: "hello.txt")
+  }
+}
